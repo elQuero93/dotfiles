0e8dac31f8932c15679210806ce9e55c6e6dcab8
el.Quero <riccardo.quercetti.93@gmail.com>
2021-07-21 10:20:52 +0200

    [ADD] locale systemwide configuration

---
+++ systemwide_settings/apply.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#
+# Locale
+#
+
+sudo cp locale.gen locale.conf /etc/
+
+# Setting default keyboard layout
+# Keyboard formats reference: https://en.wikipedia.org/wiki/IBM_PC_keyboard#Keyboard_layouts
+localectl set-x11-keymap us,it pc104 intl, grp:alt_space_toggle
